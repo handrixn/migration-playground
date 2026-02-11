4cc1c813d0cce3712ef9a2ec20c8cb910ba05bb1
candra handrian <34035457+handrixn@users.noreply.github.com>
2026-02-11 14:02:12 +0700

    add file 20260211_070209_2_TAX_METHOD_rollback.sql

---
+++ short-term/20260211_070209_2_TAX_METHOD_rollback.sql
@@ -0,0 +1,6 @@
+UPDATE agents
+SET tax_config_tax_calculation_method = 'pph21_ter_gross'
+WHERE id = 162578;
+UPDATE agents
+SET tax_config_tax_calculation_method = 'pph21_ter_gross'
+WHERE id = 123;
