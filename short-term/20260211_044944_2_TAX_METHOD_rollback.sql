6cb08248ec59c77e1b02acd25f996fdd356a5a67
Candra Handrian <candra@staffinc.co>
2026-02-11 11:54:15 +0700

    reset branch

--- short-term/20260211_044944_2_TAX_METHOD_rollback.sql
+++
@@ -1,6 +0,0 @@
-UPDATE agents
-SET tax_config_tax_calculation_method = 'pph21_ter_gross'
-WHERE id = 162578;
-UPDATE agents
-SET tax_config_tax_calculation_method = 'pph21_ter_gross'
-WHERE id = 123;
--- short-term/20260211_045040_1_TAX_METHOD_rollback.sql
+++
@@ -1,3 +0,0 @@
-UPDATE agents
-SET tax_config_tax_calculation_method = 'pph21_kemitraan_gross_up'
-WHERE id = 188642;
